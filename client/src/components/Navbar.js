d190dc3b043c34547a329c46ab050f3ff363329a
Cédric RENAIS <cedric.renais@gmail.com>
2022-01-24 19:21:28 +0100

    v0.048 : Improvement of the responsive design
    After mentoring session

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,7 @@
 import React, { useContext } from 'react';
 import HomeIcon from '@mui/icons-material/Home';
 import PersonIcon from '@mui/icons-material/Person';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import Signout from './Sign/signout';
 import { AuthContext } from '../helpers/authContext';
 //----------------------------------------//
@@ -15,6 +15,9 @@ function Navbar() {
   // Declaration of the useState hook //
   //----------------------------------//
   const { authState } = useContext(AuthContext);
+  const profil = () => {
+    window.location.replace(`/user/${authState.id}`);
+  };
   //-------------//
   // Virtual DOM //
   //-------------//
@@ -29,9 +32,11 @@ function Navbar() {
         <NavLink to={'/home'}>
           <HomeIcon aria-label="bouton accueil" className="navbar_icon" />
         </NavLink>
-        <NavLink to={`/user/${authState.id}`}>
-          <PersonIcon aria-label="bouton profil" className="navbar_icon" />
-        </NavLink>
+        <PersonIcon
+          onClick={profil}
+          aria-label="bouton profil"
+          className="navbar_icon"
+        />
         <Signout />
       </div>
     </nav>
